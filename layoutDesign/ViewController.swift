38c29db30ff4ec23a20bf655d4553aad0338889a
Duc Vu <de.yd315@gmail.com>
2018-05-28 11:09:30 -0500

    init commit

--- layoutDesign/ViewController.swift
+++ layoutDesign/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let mainView: UIView = {
+        let myView = UIView()
+        myView.backgroundColor = .yellow
+        return myView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(mainView)
+        mainView.anchor(top: view.topAnchor, bottom: view.bottomAnchor, leading: view.leadingAnchor, trailing: view.trailingAnchor, padding: .init(top: 10, left: 10, bottom: -10, right: -10))
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +32,28 @@ class ViewController: UIViewController {
 
 }
 
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: padding.bottom).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: padding.right).isActive = true
+        }
+        
+        
+        
+    }
+}
