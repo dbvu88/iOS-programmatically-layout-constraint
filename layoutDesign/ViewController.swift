5c1eedc40af3366ace118988b903f6b5fc88d604
Duc Vu <de.yd315@gmail.com>
2018-05-28 07:28:27 -0500

    Initial Commit

---
+++ layoutDesign/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  layoutDesign
+//
+//  Created by Duc Vu on 5/28/18.
+//  Copyright © 2018 Duc Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
